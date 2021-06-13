bb61b7b0dd357cf9a08dd8acf2610d211a49d03d
@GustavoNunez <guga.nd@hotmail.com>
2021-06-13 00:56:33 -0500

    ola

---
+++ MovsIniSesion.js
@@ -0,0 +1,4 @@
+  function registrar() {
+      window.location='registrar.html';
+  }
+        window.onload(alert('Para ver el contenido del menú, deberas de iniciar sesión.'));
--- firebase.js
+++ firebase.js
@@ -1,2 +1,29 @@
 
- 
+ var firebaseConfig = {
+      apiKey: "AIzaSyCDTiJ5iRF7FvH8ndogLPy3mvXwEJFfcGA",
+      authDomain: "delipollo-4fbc6.firebaseapp.com",
+      projectId: "delipollo-4fbc6",
+      storageBucket: "delipollo-4fbc6.appspot.com",
+      messagingSenderId: "552785050364",
+      appId: "1:552785050364:web:5976c21707fd35bcf4b579"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    
+    
+    const formaingresar = document.getElementById('register');
+    formaingresar.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let correo = formaingresar['email'].value;
+      let contrasena = formaingresar['password'].value;
+
+      firebase.auth().signInWithEmailAndPassword(correo, contrasena).then(cred => {
+        formaingresar.reset();
+        window.location='Menu.html';
+        alert('Si iniciaste sesión');
+      }).catch(err => {
+        alert('No pudiste iniciar sesión');
+        console.log(err);
+      });
+
+    });
